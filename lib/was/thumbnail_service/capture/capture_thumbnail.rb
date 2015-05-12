2bc66814551201b4266a6db1491dfe8867f163d2
Ahmed AlSum <aalsum@stanford.edu>
2015-05-12 16:17:03 -0700

    Add jp2 support

--- lib/was/thumbnail_service/capture/capture_thumbnail.rb
+++ lib/was/thumbnail_service/capture/capture_thumbnail.rb
@@ -12,10 +12,14 @@ module Was
         end
         
         def process_thumbnail
-          result = capture 
-          if result.length > 0 && result.starts_with('#FAIL#') then
+          result = capture
+          if (result.length > 0 && result.starts_with?('#FAIL#')) then
             if File.exist?(@temporary_file) then File.delete(@temporary_file) end
-            raise "Thumbnail for memento #{@memento_uri} can't be generated."
+            raise "Thumbnail for memento #{@memento_uri} can't be generated.\n#{result}"
+          end
+          if Rails.configuration.jp2_required then
+              Assembly::Image.new(@temporary_file+'.jpeg').create_jp2(:output=>@temporary_file+".jp2")
+              FileUtils.rm @temporary_file+'.jpeg'
           end
           save_to_stack
           update_database
@@ -25,10 +29,6 @@ module Was
           result = ""
           begin
             result = Phantomjs.run(Rails.configuration.phantom_js_script, @memento_uri, @temporary_file+".jpeg")
-            if Rails.configuration.jp2_required then
-              Assembly::Image.new(@temporary_file+'.jpeg').create_jp2(:output=>@temporary_file+".jp2")
-              FileUtils.rm @temporary_file+'.jpeg'
-            end
           rescue Exception => e 
             result = result +"\nException in generating thumbnail. #{e.message}\n#{e.backtrace.inspect}"
           end
