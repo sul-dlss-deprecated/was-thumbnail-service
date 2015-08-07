e74c0df6c9247a0ade5912dc92986adfbbb3e8a0
Ahmed AlSum <aalsum@stanford.edu>
2015-08-07 15:57:02 -0700

    Write RSpec for controllers

---
+++ spec/controllers/jobs_controller_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe JobsController, :type => :controller do
+
+  describe 'GET retry' do
+    it 'returns 404 if there is no id passed' do
+      get :retry
+      expect(response).to have_http_status(404)
+    end
+    it 'retries a job from delayed job queue' do
+      job = Delayed::Job.create({:handler=>''})
+      expect(Delayed::Job.all.length).to eq(1)
+      allow_any_instance_of(Delayed::Worker).to receive(:run).and_return(true)
+      get :retry,  {:id=> job.id}
+      expect(response).to have_http_status(:success)
+    end
+    it 'passes the job id to the find method' do
+      expect(Delayed::Job).to receive(:find).with("1234").and_return( Delayed::Job.create({:id=>1234, :handler=>''}))
+      get :retry,  {:id=> 1234}
+    end
+    it 'returns 404 if the requested job is not in delayed job queue' do
+      expect(Delayed::Job.all.length).to eq(0)
+      get :retry,  {:id=> 1234}
+      expect(response).to have_http_status(404)
+      expect(Delayed::Job.all.length).to eq(0)
+    end
+  end
+
+  describe 'GET remove' do
+    it 'deletes a job from delayed job queue' do
+      job = Delayed::Job.create({:handler=>''})
+      expect(Delayed::Job.all.length).to eq(1)
+      
+      get :remove,  {:id=> job.id}
+      expect(response).to have_http_status(:success)
+      expect(Delayed::Job.all.length).to eq(0)
+    end
+    it 'passes the job id to the delete method' do
+      expect(Delayed::Job).to receive(:delete).with("1234").and_return(1)
+      get :remove,  {:id=> 1234}
+      expect(response).to have_http_status(:success)
+    end
+    it 'returns 404 if the requested job is not in delayed job queue' do
+      expect(Delayed::Job.all.length).to eq(0)
+      get :remove,  {:id=> 1234}
+      expect(response).to have_http_status(404)
+      expect(Delayed::Job.all.length).to eq(0)
+    end
+    it 'returns 404 if there is no id passed' do
+      get :remove
+      expect(response).to have_http_status(404)
+    end
+  end
+
+end
--- spec/controllers/thumbnails_controller_spec.rb
+++ spec/controllers/thumbnails_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::ThumbnailsController, :type => :controller do
       expect(response).to have_http_status(404)
     end
     it 'returns 404 if the druid_id parameter not found' do
-      get :list, {:druid_id=>'ab123cd4567'}
+      get :list, {:druid_id => 'ab123cd4567', :format => 'json'}
       expect(assigns['seed_uri']).to be_nil
       expect(response).to have_http_status(404)
     end
@@ -18,26 +18,25 @@ RSpec.describe Api::ThumbnailsController, :type => :controller do
       expect(response).to have_http_status(404)
     end
     it 'assigns druid_id objects for available seed uri with this druid' do
-      seed_uri = SeedUri.create({:id=>100, :uri=>'http://test1.edu/', :druid_id=>'druid:ab123cd4567'})
-      get :list, {:druid_id=>'druid:ab123cd4567', :format=>'json'}
+      seed_uri = SeedUri.create({:id=>100, :uri=>'http://test1.edu/', :druid_id => 'druid:ab123cd4567'})
+      get :list, {:druid_id => 'druid:ab123cd4567', :format => 'json'}
       expect(assigns['seed_uri']).to eq(seed_uri)
       expect(assigns['seed_uri'][:druid_id]).to eq('druid:ab123cd4567')
       expect(assigns['memento_records']).to eq([])
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq('application/json')
-    end    
+    end
     it 'assigns druid_id and memento_records for seed uri with available memento' do
-      seed_uri = SeedUri.create({:id=>100, :uri=>'http://test1.edu/', :druid_id=>'druid:ab123cd4567'})
-      memento1 = Memento.create({ :uri_id=>100, :is_selected => 1, :is_thumbnail_captured => 1})
+      seed_uri = SeedUri.create({:id=>100, :uri=>'http://test1.edu/', :druid_id => 'druid:ab123cd4567'})
+      memento1 = Memento.create( :uri_id=>100, :is_selected => 1, :is_thumbnail_captured => 1)
       memento2 = Memento.create({ :uri_id=>100, :is_selected => 1, :is_thumbnail_captured => 1})
       memento3 = Memento.create({ :uri_id=>100, :is_selected => 1, :is_thumbnail_captured => 0})
-      get :list, {:druid_id=>'druid:ab123cd4567', :format=>'json'}
+      get :list, {:druid_id => 'druid:ab123cd4567', :format => 'json'}
       expect(assigns['seed_uri']).to eq(seed_uri)
       expect(assigns['seed_uri'][:druid_id]).to eq('druid:ab123cd4567')
-      expect(assigns['memento_records']).to eq([memento1,memento2])
+      expect(assigns['memento_records']).to eq([memento1, memento2])
       expect(response).to have_http_status(200)
       expect(response.content_type).to eq('application/json')
     end
   end
-
 end
