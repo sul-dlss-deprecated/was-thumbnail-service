10731dae50069fae1d0318be3a2cea7ba41b146e
Chris Beer <cabeer@stanford.edu>
2018-02-21 13:35:28 -0800

    Update to use the static codeclimate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 
 require 'coveralls'
 Coveralls.wear!
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
-  CodeClimate::TestReporter::Formatter,
   Coveralls::SimpleCov::Formatter
 ]
 
