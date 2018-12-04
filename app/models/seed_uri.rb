ff3d8bbf7b6256f32facc29373dd364de7ab5511
Justin Coyne <jcoyne@justincoyne.com>
2018-12-04 09:39:21 -0600

    Update rails to 5.0

--- app/models/seed_uri.rb
+++ app/models/seed_uri.rb
@@ -1,4 +1,4 @@
-class SeedUri < ActiveRecord::Base
+class SeedUri < ApplicationRecord
   scope :seed_info, lambda { 
                       joins('LEFT OUTER JOIN (select sum(mementos.is_thumbnail_captured) as captured, count(mementos.is_selected) as no_mementos, mementos.uri_id from mementos group by uri_id) as' \
                ' gmementos on gmementos.uri_id = seed_uris.id').select('seed_uris.druid_id','IfNull(gmementos.captured,0) as captured','seed_uris.uri','IfNull(gmementos.no_mementos,0) as no_mementos')
