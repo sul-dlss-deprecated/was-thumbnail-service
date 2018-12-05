037d6d1d5ea4ff45f10f4eedda1faa2d5b95bc25
Justin Coyne <jcoyne@justincoyne.com>
2018-12-05 16:29:45 -0600

    replace deprecated before_filter with before_action

--- app/controllers/api/thumbnails_controller.rb
+++ app/controllers/api/thumbnails_controller.rb
@@ -1,6 +1,6 @@
 module Api
   class ThumbnailsController < ApplicationController
-    before_filter :find_uri, only: [:list]
+    before_action :find_uri, only: [:list]
     respond_to :json
 
     #https://was-thumbnail.example.org/api/v1/was/thumbnails/druid_id/jx731tz7613?format=json
@@ -11,16 +11,16 @@ module Api
       @thumb_size = get_thumb_size(params[:thumb_size])
     end
 
-    private 
+    private
     def find_uri
       if params[:druid_id].present?
-        @seed_uri = SeedUri.find_by(druid_id: params[:druid_id]) 
+        @seed_uri = SeedUri.find_by(druid_id: params[:druid_id])
         render nothing: true, status: :not_found unless @seed_uri.present?
       elsif params[:uri].present?
-        @seed_uri = SeedUri.find_by(uri: params[:uri]) 
+        @seed_uri = SeedUri.find_by(uri: params[:uri])
         render nothing: true, status: :not_found unless @seed_uri.present?
       else
-        render nothing: true, status: :not_found 
+        render nothing: true, status: :not_found
       end
     end
     def get_thumb_size(param_thumb_size)
