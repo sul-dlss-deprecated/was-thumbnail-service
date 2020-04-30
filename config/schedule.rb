44366dd69791db8b5b1806a578d54fd1b756b326
Jeremy Nelson <jermnelson@gmail.com>
2020-04-29 18:43:56 -0600

    Added custom job_type to prevent deprecated emails

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+job_type :no_warnings_rake, "cd :path && :environment_variable=:environment RUBYOPT='-W0' bundle exec rake :task --silent :output"
+
 every 30.minutes do
-  rake 'was_thumbnail_service:run_thumbnail_monitor'
+  no_warnings_rake 'was_thumbnail_service:run_thumbnail_monitor'
 end
